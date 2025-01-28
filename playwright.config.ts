8ca3594857a807750189013c609352cbde38728f
Nikki Sato <54095031+nikkisato@users.noreply.github.com>
2025-01-27 20:19:23 -0800

    removing microsoft edge

--- playwright.config.ts
+++ playwright.config.ts
@@ -63,10 +63,10 @@ export default defineConfig({
     },
 
     /* Test against branded browsers. */
-    {
-      name: 'Microsoft Edge',
-      use: { ...devices['Desktop Edge'] },
-    },
+    // {
+    //   name: 'Microsoft Edge',
+    //   use: { ...devices['Desktop Edge'] },
+    // },
     {
       name: 'Google Chrome',
       use: { ...devices['Desktop Chrome'], browserName: 'chromium' },
